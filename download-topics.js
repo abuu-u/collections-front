b876e600c131c4116ab3cd47727f722fe1ff7956
abuu <shayxislomovabdurahmon@gmail.com>
2022-07-07 14:40:12 +0500

    use url from env

--- download-topics.js
+++ download-topics.js
@@ -3,7 +3,7 @@
 const fs = require('fs')
 const path = require('path')
 const http = require('http')
-const url = 'http://localhost:5129/CollectionsApi' + '/Collections/topics'
+const url = process.env.NEXT_PUBLIC_API + '/Collections/topics'
 http
   .get(url, (res) => {
     let data = ''
